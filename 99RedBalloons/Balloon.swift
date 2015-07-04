739ab5f7bbdf3f568dd163fd3a83a2b2fe90d6ed
bitfountain-student <bitfountain@slowchat.us>
2015-07-04 05:03:21 -0700

    making progress

---
+++ 99RedBalloons/Balloon.swift
@@ -0,0 +1,15 @@
+//
+//  Balloon.swift
+//  99RedBalloons
+//
+//  Created by Richard Gambord on 7/3/15.
+//  Copyright (c) 2015 bitfountain-student. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Balloon {
+    var image = UIImage(named:"")
+    var count = 0
+}
